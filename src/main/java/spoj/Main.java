7e42d3b8f7c4cf343609a5fbef8adc0399813a03
jeddyn <zar1117@o2.pl>
2017-05-27 23:06:13 +0200

    End
    End

--- src/main/java/spoj/Main.java
+++ src/main/java/spoj/Main.java
@@ -7,16 +7,15 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        long n, current = 0, highest = 0;       //n = number of cities
+        long n, current = 0, highest = 0;
 
         Scanner scan = new Scanner(System.in);
         n = scan.nextInt();
+
         if (n >= 1 && n <= 100000) {
+
             GetSeries function = new GetSeries(scan, n);
-            //getHighest(long n, long current, long highest, Scanner scan)
-
             System.out.println(function.getHighest());
-
 
         } else System.exit(-1);
     }
